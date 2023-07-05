af986afe0e395e1b9a6aff76a989c86abb6e112b
Arseniy Ivanov <freeatnet@freeatnet.com>
2023-07-05 18:41:01 +0300

    initial config

---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { e } from "~/server/edgedb";
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+  currentTime: publicProcedure.query(async ({ ctx: { edgedbClient } }) => {
+    const query = e.select(e.cast(e.str, e.datetime_current()));
+    const time = await query.run(edgedbClient);
+
+    return time;
+  }),
+});
